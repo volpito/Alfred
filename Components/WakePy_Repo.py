44e89b16e98784a7477bb4c4b2ed41e2b12971a7
RIGOT Maxime <69733938+volpito@users.noreply.github.com>
2026-05-16 18:03:31 +0200

    Update WakePy_Repo.py to add name var

--- Components/WakePy_Repo.py
+++ Components/WakePy_Repo.py
@@ -3,21 +3,26 @@ from PIL import Image, ImageDraw, ImageFont
 from waveshare_epd import epd2in13_V4
 
 class ScreenTest:
-  # 1. Setup
-  epd = epd2in13_V4.EPD()
-  epd.init()
-  epd.Clear()
-  # 2. Create Image (using e-paper dimensions)
-  image = Image.new('1', (epd.width, epd.height), 255) #255 = white background
-  draw = ImageDraw.Draw(image)
-  font = ImageFont.load_default()
-  # 3. Draw directly
-  draw.text((10, 10), 'Hello Boss', font=font, fill=0)
-  # 4. Push to screen
-  epd.display(epd.getbuffer(image))
-  # 5. Cleanup
-  time.sleep(2)
-  epd.sleep()
+
+  def __init__(self, name):
+        self.name = name
+    
+  def Run(self):
+    # 1. Setup
+    epd = epd2in13_V4.EPD()
+    epd.init()
+    epd.Clear()
+    # 2. Create Image (using e-paper dimensions)
+    image = Image.new('1', (epd.width, epd.height), 255) #255 = white background
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.load_default()
+    # 3. Draw directly
+    draw.text((10, 10), 'Hello Boss', font=font, fill=0)
+    # 4. Push to screen
+    epd.display(epd.getbuffer(image))
+    # 5. Cleanup
+    time.sleep(2)
+    epd.sleep()
 
 if __name__ == '__main__':
     ScreenTest().Run()
